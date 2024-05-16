1a2095e34354f15578ba25903c4fe2b147c3ab62
speed <153678863+ttahaa1@users.noreply.github.com>
2024-05-16 14:51:35 +0300

    Update tcrep1.py

--- tcrep1.py
+++ tcrep1.py
@@ -5,7 +5,7 @@ api_id = '22379637'
 api_hash = '7a5777d4bd0480856f9e7b129acc7a9f'
 session_string = 'BAFEO3QAgkEJI1MmV8sUSTdWXsAgpeJbXTHCPHFaonehGZd8flub_RgYgX8rOBTDKMYcp_NxyAHUzcxXx5Tm_9151SkT8ntmlodNYSmGPvYQJizS0zbuHU6nZCjhKwWbfdBbEP9ez6x94nmf_lb6bWygn4J5Jv5A8709NUtmKpldhhg3P8Z0TSHYr2q3N9GvDriaoylnKOX-4Y8LiMpIolAJxI1TkB2JXDz8UhrIW_L-vQ51uh2oJB3x3EUDvY3nkF4Zg8NXhwlNYEzlSOpbe_JK4IefHZJUu-3_KFO7SNWAxlZLYTx_rR3VNDK1QR30J_VKpyJdLTf8_gQgTdb3PCYCzxBcoAAAAAGJ8ONCAA'
 
-start_number = 1
+start_number = 16
 end_number = 28  
 
 links = [f"https://t.me/c/2027544784/{i}" for i in range(start_number, end_number + 1)]
